6837736a949510b38057f0b684bf07ff9ab967e9
mailme.kirpal@gmail.com <kirpaltpss@gmail.com>
2022-03-28 10:24:46 +0530

    Root Chages Commit

--- redis.php
+++ redis.php
@@ -5,6 +5,7 @@
 	error_reporting(E_ALL);
 
 	require 'Predis/Autoloader.php';
+	
 	Predis\Autoloader::register();
 
 	$parameters = 'tcp://127.0.0.1:6379';
@@ -12,10 +13,11 @@
 	$redis = new Predis\Client($parameters);
 	
 	$redis_key = "SMSAUTH:9876543210";
+
 	$sms_auth_num = sprintf('%04d',mt_rand(1000,9999));
 	
 	echo $redis_result = $redis->set($redis_key, $sms_auth_num);
-	
+
 	echo $redis->expire($redis_key, 180);
 
 	echo $redis->get($redis_key);
